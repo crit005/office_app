82bebb6bcf4f953ba3fc1bd4a28c344d421950e4
Chhoun Sokonthea <sokonthea.chhoun@yahoo.com>
2022-05-30 19:56:34 +0700

    add payment muneu

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -12,17 +12,17 @@
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
                 @auth
-                    <img src="{{ auth()->user()->photoUrl }}" class="img-circle elevation-2"
-                        style="width: 34px; height: 34px;" alt="User Image">
+                <img src="{{ auth()->user()->photoUrl }}" class="img-circle elevation-2"
+                    style="width: 34px; height: 34px;" alt="User Image">
                 @endauth
                 @guest
-                    <img src="{{ asset('backend/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2"
-                        alt="User Image">
+                <img src="{{ asset('backend/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2"
+                    alt="User Image">
                 @endguest
             </div>
             <div class="info">
                 <a href="#" class="d-block">@auth
-                        {{ auth()->user()->name }}
+                    {{ auth()->user()->name }}
                     @endauth
                 </a>
             </div>
@@ -51,17 +51,6 @@
                         <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>
                             Dashboard
-                        </p>
-                    </a>
-                </li>
-
-                {{-- Expand Menu --}}
-                <li class="nav-item">
-                    <a href="{{ route('dashboard') }}"
-                        class="nav-link {{ request()->is('expand') ? 'active' : '' }}">
-                        <i class="nav-icon fas fa-shopping-cart"></i>
-                        <p>
-                            Expand
                         </p>
                     </a>
                 </li>
@@ -99,44 +88,62 @@
                     </a>
                 </li>
 
-
-                {{-- Setting menu --}}
-                <li class="nav-item {{ Request::segment(1)=='setting'? 'menu-open' : 'menu-close'}}">
-                    <a href="#" class="nav-link {{ Request::segment(1)=='setting'? 'active' : ''}}">
-                        <i class="nav-icon fas fa-cog"></i>
+                {{-- Paynebt menu --}}
+                {{-- <li class="nav-item {{ Request::segment(1)=='payment'? 'menu-open' : 'menu-close'}}"> --}}
+                <li class="nav-item menu-open">
+                    <a href="#" class="nav-link {{ Request::segment(1)=='payment'? 'active' : ''}}">
+                        <i class="nav-icon fas fa-shopping-cart"></i>
                         <p class="red">
-                            Setting
+                            Pament
                             <i class="right fas fa-angle-down"></i>
                         </p>
                     </a>
+
                     <ul class="nav nav-treeview">
 
-                        {{-- Depatment Menu --}}
+                        {{-- Expand Menu --}}
                         <li class="nav-item">
-                            <a href="{{route('setting.depatments')}}" class="nav-link {{ request()->is('setting/depatments') ? 'active' : '' }}">
-                                <i class="vav-icon fas fa-sitemap ml-4"></i>
+                            <a href="{{ route('dashboard') }}"
+                                class="nav-link {{ request()->is('expand') ? 'active' : '' }}">
+                                <i class="nav-icon fas fa-cart-plus  ml-4"></i>
                                 <p>
-                                    Depatment
+                                    Expand
                                 </p>
                             </a>
                         </li>
 
-                        {{-- Currency Menu --}}
+                        {{-- Add Cash Menu --}}
                         <li class="nav-item">
-                            <a href="{{route('setting.currencies')}}" class="nav-link {{ request()->is('setting/currencies') ? 'active' : '' }}">
-                                <i class="nav-icon fas fa-hand-holding-usd ml-4"></i>
+                            <a href="{{ route('dashboard') }}"
+                                class="nav-link {{ request()->is('expand') ? 'active' : '' }}">
+                                {{-- <sup><i class="fas fa-plus-circle ml-4" style="font-size: .5rem"></i></sup>
+                                <i class="fas fa-dollar-sign mr-2"></i> --}}
+                                <i class="nav-icon fas fa-donate ml-4"></i>
                                 <p>
-                                    Currencies
+                                    Add Cash
                                 </p>
                             </a>
                         </li>
 
-                        {{-- Item Menu --}}
+                        {{-- Exchange Menu --}}
                         <li class="nav-item">
-                            <a href="{{route('setting.items')}}" class="nav-link {{ request()->is('setting/items') ? 'active' : '' }}">
-                                <i class="nav-icon fas fa-dice-d6 ml-4"></i>
+                            <a href="{{ route('dashboard') }}"
+                                class="nav-link {{ request()->is('expand') ? 'active' : '' }}">
+                                {{-- <i class="nav-icon fas fa-exchange-alt  ml-4"></i> --}}
+                                <i class="nav-icon fas fa-sync ml-4"></i>
                                 <p>
-                                    Items
+                                    Exchange
+                                </p>
+                            </a>
+                        </li>
+
+                        {{-- Transfer Menu --}}
+                        <li class="nav-item">
+                            <a href="{{ route('dashboard') }}"
+                                class="nav-link {{ request()->is('expand') ? 'active' : '' }}">
+                                <i class="nav-icon fas fa-random  ml-4"></i>
+                                <p>
+                                    Transfer
                                 </p>
                             </a>
                         </li>
@@ -151,6 +158,56 @@
                                 </p>
                             </a>
                         </li>
+
+                    </ul>
+                </li>
+
+
+                {{-- Setting menu --}}
+                {{-- <li class="nav-item {{ Request::segment(1)=='setting'? 'menu-open' : 'menu-close'}}"> --}}
+                <li class="nav-item menu-open">
+                    <a href="#" class="nav-link {{ Request::segment(1)=='setting'? 'active' : ''}}">
+                        <i class="nav-icon fas fa-cog"></i>
+                        <p class="red">
+                            Setting
+                            <i class="right fas fa-angle-down"></i>
+                        </p>
+                    </a>
+
+                    <ul class="nav nav-treeview">
+
+                        {{-- Depatment Menu --}}
+                        <li class="nav-item">
+                            <a href="{{route('setting.depatments')}}"
+                                class="nav-link {{ request()->is('setting/depatments') ? 'active' : '' }}">
+                                <i class="vav-icon fas fa-sitemap ml-4"></i>
+                                <p>
+                                    Depatment
+                                </p>
+                            </a>
+                        </li>
+
+                        {{-- Currency Menu --}}
+                        <li class="nav-item">
+                            <a href="{{route('setting.currencies')}}"
+                                class="nav-link {{ request()->is('setting/currencies') ? 'active' : '' }}">
+                                <i class="nav-icon fas fa-hand-holding-usd ml-4"></i>
+                                <p>
+                                    Currencies
+                                </p>
+                            </a>
+                        </li>
+
+                        {{-- Item Menu --}}
+                        <li class="nav-item">
+                            <a href="{{route('setting.items')}}"
+                                class="nav-link {{ request()->is('setting/items') ? 'active' : '' }}">
+                                <i class="nav-icon fas fa-dice-d6 ml-4"></i>
+                                <p>
+                                    Items
+                                </p>
+                            </a>
+                        </li>                        
 
                         {{-- User Menu --}}
                         <li class="nav-item">
@@ -182,8 +239,8 @@
                 <li class="nav-item">
                     <form action="{{ route('logout') }}" method="POST">
                         @csrf
-                        <a href="{{ route('logout') }}"
-                            onclick="event.preventDefault(); this.closest('form').submit();" class="nav-link">
+                        <a href="{{ route('logout') }}" onclick="event.preventDefault(); this.closest('form').submit();"
+                            class="nav-link">
                             <i class="nav-icon fas fa-sign-out-alt"></i>
                             <p>
                                 Logout
