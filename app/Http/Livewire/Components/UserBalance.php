dc9841362bf3b1098b239fad479b6064697768a5
Chhoun Sokonthea <sokonthea.chhoun@yahoo.com>
2022-06-07 12:24:23 +0700

    error array object

--- app/Http/Livewire/Components/UserBalance.php
+++ app/Http/Livewire/Components/UserBalance.php
@@ -3,18 +3,34 @@
 namespace App\Http\Livewire\Components;
 
 use App\Models\CashTransaction;
+use App\Models\Currency;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 class UserBalance extends Component
 {
     public function render()
     {
-        $balances = CashTransaction::where('owner', '=', auth()->user()->id)
-            ->groupBy('currency_id')
-            ->selectRaw('SUM(amount) as current_balance')
-            // ->selectRaw('SUM(amount) as current_balance, currency_id')
-            // ->pluck('current_balance','currency_id');
-            ->get();
+        $balances = DB::select(
+            "
+            SELECT SUM(cash_transactions.amount) as current_balance, currencies.symbol
+            From cash_transactions INNER JOIN currencies ON cash_transactions.currency_id = currencies.id
+            WHERE cash_transactions.owner = ? 
+            GROUP BY cash_transactions.currency_id ,currencies.symbol
+        ",
+            [
+                // auth()->user()->id
+                2
+            ]
+        );
+        if (!$balances) {
+            $currencies = Currency::where('status', '=', 'ENABLED')->orderBy('position', 'asc')->get();
+            foreach ($currencies as $index => $currency) {
+                array_push($balances, {'current_balance':0, 'symbol' :  $currency->symbol});
+            }
+        }
+        
+        // $balances=CashTransaction::where('owner','=',auth()->user()->id)->latest()->get()->groupBY('currency_id');
 
         return view('livewire.components.user-balance', ['balances' => $balances]);
     }
--- resources/views/livewire/components/user-balance.blade.php
+++ resources/views/livewire/components/user-balance.blade.php
@@ -1,7 +1,12 @@
 <div>
     <span class="d-block text-white">Your Current Balance</span>
+   
+    @dump(gettype($balances))
+    @dump($balances)
     @foreach ($balances as $balance)
-        @dump($balance)
+    {{-- {{$balance['current_balance'] .$balance['symbol']}} --}}
     @endforeach
+    
+
     <span class="text-black">861285 ฿ / 6538 $</span>
 </div>
