22a32ac1328cecb96c7f0454d7830d0cb9bb1fb0
crit005 <sokonthea.chhoun@yahoo.com>
2022-06-07 14:35:26 +0700

    fixed array error

--- app/Http/Livewire/Components/UserBalance.php
+++ app/Http/Livewire/Components/UserBalance.php
@@ -15,21 +15,21 @@ class UserBalance extends Component
             "
             SELECT SUM(cash_transactions.amount) as current_balance, currencies.symbol
             From cash_transactions INNER JOIN currencies ON cash_transactions.currency_id = currencies.id
-            WHERE cash_transactions.owner = ? 
+            WHERE cash_transactions.owner = ?
             GROUP BY cash_transactions.currency_id ,currencies.symbol
         ",
             [
-                // auth()->user()->id
-                2
+                auth()->user()->id
             ]
         );
         if (!$balances) {
             $currencies = Currency::where('status', '=', 'ENABLED')->orderBy('position', 'asc')->get();
             foreach ($currencies as $index => $currency) {
-                array_push($balances, {'current_balance':0, 'symbol' :  $currency->symbol});
+                array_push($balances, ['current_balance'=>0, 'symbol' =>  $currency->symbol]);
             }
+            $balances = json_decode(json_encode($balances)) ;
         }
-        
+
         // $balances=CashTransaction::where('owner','=',auth()->user()->id)->latest()->get()->groupBY('currency_id');
 
         return view('livewire.components.user-balance', ['balances' => $balances]);
--- resources/views/livewire/components/user-balance.blade.php
+++ resources/views/livewire/components/user-balance.blade.php
@@ -1,12 +1,10 @@
 <div>
     <span class="d-block text-white">Your Current Balance</span>
-   
-    @dump(gettype($balances))
-    @dump($balances)
-    @foreach ($balances as $balance)
-    {{-- {{$balance['current_balance'] .$balance['symbol']}} --}}
-    @endforeach
-    
-
-    <span class="text-black">861285 ฿ / 6538 $</span>
+    <span class="text-black">
+        {{-- @dump(gettype($balances))
+        @dump($balances) --}}
+        @foreach ($balances as $balance)
+        {{$balance->current_balance .$balance->symbol}}
+        @endforeach
+    </span>
 </div>
